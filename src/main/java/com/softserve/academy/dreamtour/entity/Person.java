78978e44d5fd44f92b4d92883352062bb19ddb51
Rostyk Hlynka <hlynka1608@gmail.com>
2019-05-16 13:16:24 +0300

    add enum

--- src/main/java/com/softserve/academy/dreamtour/entity/Person.java
+++ src/main/java/com/softserve/academy/dreamtour/entity/Person.java
@@ -2,6 +2,8 @@ package com.softserve.academy.dreamtour.entity;
 
 import java.util.Objects;
 
+import com.softserve.academy.dreamtour.enums.PersonType;
+
 public class Person {
     
     private Long idPerson;
@@ -9,18 +11,21 @@ public class Person {
     private String password;
     private String firstName;
     private String lastName;
+    private PersonType personType;
     
     public Person() { }
 
-    public Person(Long idPerson, String username, String password, String firstName, String lastName) {
+    public Person(Long idPerson, String username, String password, String firstName, String lastName,
+            PersonType personType) {
         this.idPerson = idPerson;
         this.username = username;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.personType = personType;
     }
 
-    
+
     public Long getIdPerson() {
         return idPerson;
     }
@@ -61,7 +66,15 @@ public class Person {
         this.lastName = lastName;
     }
 
+    public PersonType getPersonType() {
+        return personType;
+    }
 
+    public void setPersonType(PersonType personType) {
+        this.personType = personType;
+    }
+
+    
     @Override
     public boolean equals(Object o) {
 
@@ -81,16 +94,19 @@ public class Person {
 
     @Override
     public int hashCode() {
+        
         return Objects.hash(idPerson, username, password, firstName, lastName);
     }
 
     @Override
     public String toString() {
+        
         return "Person [" +
                 "idPerson=" + idPerson +
                 ", username=" + username +
                 ", firstName=" + firstName +
                 ", lastName=" + lastName +
+                ", personType=" + personType +
                 "]";
     }
     
