09b08bdf809250951e8d38a4c94f3d286e641581
Rostyk Hlynka <hlynka1608@gmail.com>
2019-05-16 13:31:51 +0300

    add dao

---
+++ src/main/java/com/softserve/academy/dreamtour/dao/interfaces/Dao.java
@@ -0,0 +1,14 @@
+package com.softserve.academy.dreamtour.dao.interfaces;
+
+import java.util.List;
+
+public interface Dao<T> {
+
+    List<T> getAll();
+    
+    boolean add(T t);         // C
+    T get(int id);            // R
+    boolean update(T t);      // U
+    boolean delete(int id);   // D
+
+}
