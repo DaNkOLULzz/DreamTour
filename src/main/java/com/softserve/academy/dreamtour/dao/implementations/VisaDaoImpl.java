30ea6a43daa74b345987b5e790e5e7e9bdf7c8dd
Rostyk Hlynka <hlynka1608@gmail.com>
2019-05-17 16:42:45 +0300

    dao init

---
+++ src/main/java/com/softserve/academy/dreamtour/dao/implementations/VisaDaoImpl.java
@@ -0,0 +1,35 @@
+package com.softserve.academy.dreamtour.dao.implementations;
+
+import java.util.List;
+
+import com.softserve.academy.dreamtour.dao.interfaces.IVisaDao;
+import com.softserve.academy.dreamtour.entity.Visa;
+
+public class VisaDaoImpl implements IVisaDao {
+
+    public List<Visa> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean add(Visa t) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public Visa get(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean update(Visa t) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean delete(int id) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
---
+++ src/main/java/com/softserve/academy/dreamtour/dao/interfaces/IPersonDao.java
@@ -0,0 +1,7 @@
+package com.softserve.academy.dreamtour.dao.interfaces;
+
+import com.softserve.academy.dreamtour.entity.Person;
+
+public interface IPersonDao extends IDao<Person> {
+    
+}
