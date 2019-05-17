aad64ed9598f1ea89474e785d121de5eda2174a4
Rostyk Hlynka <hlynka1608@gmail.com>
2019-05-18 01:30:54 +0300

    delete needless code

--- src/main/java/com/softserve/academy/dreamtour/controller/Main.java
+++ src/main/java/com/softserve/academy/dreamtour/controller/Main.java
@@ -1,4 +1,4 @@
-package com.softserve.academy.dreamtour.servlets;
+package com.softserve.academy.dreamtour.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,11 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Main")
+@WebServlet("/main")
 public class Main extends HttpServlet {
     
-    private static final long serialVersionUID = 1L;
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
