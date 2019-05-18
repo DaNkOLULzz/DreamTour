8df5a7af1360de3e6a18b63b81ac1d3b071c9bfb
Rostyk Hlynka <hlynka1608@gmail.com>
2019-05-18 19:31:31 +0300

    inserting user in db

--- src/main/java/com/softserve/academy/dreamtour/dao/implementations/PersonDaoImpl.java
+++ src/main/java/com/softserve/academy/dreamtour/dao/implementations/PersonDaoImpl.java
@@ -1,9 +1,15 @@
 package com.softserve.academy.dreamtour.dao.implementations;
 
-import java.util.List;
-
+import com.softserve.academy.dreamtour.config.DBConnection;
 import com.softserve.academy.dreamtour.dao.interfaces.IPersonDao;
 import com.softserve.academy.dreamtour.entity.Person;
+import com.softserve.academy.dreamtour.enums.PersonType;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.naming.NamingException;
 
 public class PersonDaoImpl implements IPersonDao {
 
@@ -12,9 +18,20 @@ public class PersonDaoImpl implements IPersonDao {
         return null;
     }
 
-    public boolean add(Person t) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean add(Person person) throws SQLException, NamingException {
+
+        String sql = "INSERT INTO person (username, pass, first_name, last_name, person_type) "
+                + "VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement ps = DBConnection.getConnection().prepareStatement(sql);
+        ps.setString(1, person.getUsername());
+        ps.setString(2, person.getPassword());
+        ps.setString(3, person.getFirstName());
+        ps.setString(4, person.getLastName());
+        ps.setString(5, "user");
+        
+        int rows = ps.executeUpdate();
+        
+        return rows > 0;
     }
 
     public Person get(int id) {
@@ -22,7 +39,7 @@ public class PersonDaoImpl implements IPersonDao {
         return null;
     }
 
-    public boolean update(Person t) {
+    public boolean update(Person person) {
         // TODO Auto-generated method stub
         return false;
     }
