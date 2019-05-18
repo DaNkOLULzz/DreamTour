6718c6dfc0ae98d330f71c85a177113d78e1a35a
Olena <diru4ova@gmail.com>
2019-05-18 23:46:54 +0300

    ICountryDao, ICityDao, CountryDaoImpl, CityDaoImpl added

--- src/main/java/com/softserve/academy/dreamtour/dao/interfaces/IDao.java
+++ src/main/java/com/softserve/academy/dreamtour/dao/interfaces/IDao.java
@@ -1,14 +1,16 @@
 package com.softserve.academy.dreamtour.dao.interfaces;
 
+import javax.naming.NamingException;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IDao<T> {
 
-    List<T> getAll();
+    List<T> getAll() throws SQLException;
     
-    boolean add(T t);         // C
-    T get(int id);            // R
-    boolean update(T t);      // U
-    boolean delete(int id);   // D
+    boolean add(T t) throws SQLException;         // C
+    T get(int id) throws SQLException, NamingException;            // R
+    boolean update(T t) throws SQLException;      // U
+    boolean delete(int id) throws SQLException;   // D
 
 }
