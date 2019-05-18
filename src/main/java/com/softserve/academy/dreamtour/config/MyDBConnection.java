a63e231ad2329fd8f6a8458d3bddd697f2eb7fff
DaNkOLULzz <danylolototskyi@gmail.com>
2019-05-18 22:00:41 +0300

    add HotelDaoImpl

--- src/main/java/com/softserve/academy/dreamtour/config/MyDBConnection.java
+++ src/main/java/com/softserve/academy/dreamtour/config/MyDBConnection.java
@@ -12,10 +12,17 @@ public class MyDBConnection {
     public static Connection getConnection() throws NamingException, SQLException {
         InitialContext ctx = new InitialContext();
         Context initCtx  = (Context) ctx.lookup("java:/comp/env");
-        DataSource ds = (DataSource) initCtx.lookup("jdbc/DreamTourDB");
+        DataSource ds = (DataSource) initCtx.lookup("jdbc/dreamtour");
         Connection conn = ds.getConnection();
         return conn;
     }
 
-
+    public static void main(String[] args) {
+        try (
+            Connection conn = MyDBConnection.getConnection()) {
+            System.out.println("Good");
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
 }
