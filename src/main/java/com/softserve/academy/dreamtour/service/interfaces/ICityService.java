3da8f0ba7fe7d5b3c07a76433582b9e90c45d26d
Olena <diru4ova@gmail.com>
2019-05-20 22:26:11 +0300

    ICityService & CityServiceImpl added

---
+++ src/main/java/com/softserve/academy/dreamtour/service/interfaces/ICityService.java
@@ -0,0 +1,22 @@
+package com.softserve.academy.dreamtour.service.interfaces;
+
+import com.softserve.academy.dreamtour.entity.City;
+
+import javax.naming.NamingException;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ICityService {
+
+    List<City> getAll() throws SQLException, NamingException;
+
+    boolean add(City city) throws SQLException, NamingException; // C
+
+    City get(int id) throws SQLException, NamingException; // R
+
+    boolean update(City city) throws SQLException, NamingException; // U
+
+    boolean delete(int id) throws SQLException, NamingException; // D
+
+    List<String> getCityNameByCountry(String countryName) throws SQLException, NamingException;
+}
