5751c05e1a81f1d910e29410fb1e9cb1d38260dc
Olena <diru4ova@gmail.com>
2019-05-20 19:44:22 +0300

    custom method added to ICityDao and its impl

--- src/main/java/com/softserve/academy/dreamtour/dao/interfaces/ICityDao.java
+++ src/main/java/com/softserve/academy/dreamtour/dao/interfaces/ICityDao.java
@@ -2,5 +2,11 @@ package com.softserve.academy.dreamtour.dao.interfaces;
 
 import com.softserve.academy.dreamtour.entity.City;
 
+import javax.naming.NamingException;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ICityDao extends  IDao<City>{
+
+    List<String> getCityByCountry(String countryName) throws SQLException, NamingException;
 }
