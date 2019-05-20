0a356a830735e16f8982060640cf982ab276b503
Rostyk Hlynka <hlynka1608@gmail.com>
2019-05-20 20:30:57 +0300

    get all visa by person

--- src/main/java/com/softserve/academy/dreamtour/dao/interfaces/IVisaDao.java
+++ src/main/java/com/softserve/academy/dreamtour/dao/interfaces/IVisaDao.java
@@ -1,7 +1,14 @@
 package com.softserve.academy.dreamtour.dao.interfaces;
 
+import com.softserve.academy.dreamtour.entity.Person;
 import com.softserve.academy.dreamtour.entity.Visa;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.naming.NamingException;
 
 public interface IVisaDao extends IDao<Visa> {
 
+    List<Visa> getAllVisaByPerson(int idPerson) throws SQLException, NamingException;
 }
