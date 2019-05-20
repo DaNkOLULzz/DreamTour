507726983da4741355a0ffa767f92e977b79b5e0
DaNkOLULzz <danylolototskyi@gmail.com>
2019-05-20 23:12:06 +0300

    add getVisaCountByCountryForPerson to IVisaDao

--- src/main/java/com/softserve/academy/dreamtour/dao/interfaces/IVisaDao.java
+++ src/main/java/com/softserve/academy/dreamtour/dao/interfaces/IVisaDao.java
@@ -2,6 +2,11 @@ package com.softserve.academy.dreamtour.dao.interfaces;
 
 import com.softserve.academy.dreamtour.entity.Visa;
 
-public interface IVisaDao extends IDao<Visa> {
+import java.sql.SQLException;
+import java.util.List;
+import javax.naming.NamingException;
 
+public interface IVisaDao extends IDao<Visa> {
+    int getVisaCountByCountryForPerson(
+        String countryName, int idPerson) throws SQLException, NamingException;
 }
