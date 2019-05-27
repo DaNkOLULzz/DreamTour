9bad993feed1f0913bf040c1c02b1c6163b47d48
Rostyk Hlynka <hlynka1608@gmail.com>
2019-05-27 18:03:08 +0300

    profile

--- src/main/java/com/softserve/academy/dreamtour/controller/ProfilePageServlet.java
+++ src/main/java/com/softserve/academy/dreamtour/controller/ProfilePageServlet.java
@@ -1,17 +1,37 @@
 package com.softserve.academy.dreamtour.controller;
 
+import com.softserve.academy.dreamtour.entity.Person;
+import com.softserve.academy.dreamtour.service.implementations.PersonServiceImpl;
+import com.softserve.academy.dreamtour.service.interfaces.IPersonService;
+
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet("/profile")
 public class ProfilePageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+
+        HttpSession session = req.getSession(false);
+        IPersonService iPersonService = new PersonServiceImpl();
+
+        try {
+
+            String currentUsername = (String) session.getAttribute("user");
+            Person person = iPersonService.getPersonByCredentials(currentUsername);
+
+            req.setAttribute("person", person);
+        } catch (SQLException | NamingException e) {
+            e.printStackTrace();
+        }
 
         req.getRequestDispatcher("pages/profile.jsp").forward(req, resp);
     }
